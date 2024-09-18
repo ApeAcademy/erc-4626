3b9a74386d5dcc48262ef225b0e867b6f5b92626
johnson2427 <blakeejohnson39@gmail.com>
2024-09-18 08:06:18 -0500

    feat: setting up dockerfile

---
+++ bots/silverback_yield.py
@@ -0,0 +1,79 @@
+import os
+
+from ape import Contract, chain, networks
+from silverback import SilverbackApp
+
+from sqlmodel import SQLModel, Field, create_engine, Session
+
+sqlite_file_name = "silverback_yield.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+app = SilverbackApp()
+
+with networks.ethereum.mainnet.use_provider("alchemy"):  # or "infura"
+    vault = Contract(os.environ.get("ERC4626_VAULT_ADDRESS"))
+
+one_share = 10 ** vault.decimals()
+engine = create_engine(sqlite_url, echo=True)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+class Transaction(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    block: str
+    sender: str
+    owner: str
+    assets: int
+    shares: int
+    timestamp: int
+    transaction_hash: str
+    transaction_type: str
+
+
+def create_transaction(log):
+    transaction = Transaction(
+        block=log.block_number,
+        sender=log.sender,
+        owner=log.args.owner,
+        assets=log.args.assets,
+        shares=log.args.shares,
+        timestamp=log.timestamp,
+        transaction_hash=log.transaction_hash,
+        transaction_type="deposit",
+    )
+    with Session(engine) as session:
+        session.add(transaction)
+        session.commit()
+        session.refresh(transaction)
+    return transaction
+
+
+# Note: Use SQLModel
+@app.on_(vault.Deposit)
+def update_database_deposit(log):
+    """
+    Update database with deposit log.
+    """
+    print(f"Deposit Event: {log}")
+    return create_transaction(log)
+
+
+@app.on_(vault.Withdraw)
+def update_database_withdraw(log):
+    """
+    Update database with withdraw log
+    """
+    print(f"Withdraw Event: {log}")
+    return create_transaction(log)
+
+
+@app.on_(chain.blocks)
+def update_shareprice(_):
+    """
+    Add price to database (Update database)
+    """
+    price = vault.convertToShares(one_share) / one_share
+    print(f"Price Event: {price}")
